aa55fc88e521dbd018da1afff19d422156050103
Meruem117 <1572677319@qq.com>
2022-02-15 20:02:52 +0800

    add windicss

---
+++ dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var common_vendor = require("../../common/vendor.js");
+const _sfc_main = /* @__PURE__ */ common_vendor.defineComponent({
+  setup(__props) {
+    common_vendor.ref("Hello");
+    return (_ctx, _cache) => {
+      return {};
+    };
+  }
+});
+wx.createPage(_sfc_main);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
-});
+  plugins: [
+    uni(),
+    WindiCSS({
+      scan: {
+        dirs: ['.'],
+        fileExtensions: ['vue', 'js', 'ts']
+      }
+    })
+  ]
+})
