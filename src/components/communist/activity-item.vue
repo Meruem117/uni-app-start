cd46dbd087399bb9ddbff88e382fd0599d28e7ff
Meruem117 <1572677319@qq.com>
2022-02-16 21:02:00 +0800

    reformat

--- src/components/communist/activity-item.vue
+++ src/components/communist/activity-item.vue
@@ -1,24 +1,24 @@
 <template>
-    <view class="base_item" @click="toActivityAttend">
-        <view class="base_p" style="width: 120rpx;">
-            <view class="base_icon">
-                <uni-icons type="flag-filled" size="30" class="m-auto"></uni-icons>
-            </view>
-        </view>
-        <view class="flex flex-col base_p" style="width: 380rpx;">
-            <view class="base_head">[{{ props.item.type }}] {{ props.item.title }}</view>
-            <view class="text-gray-400 base_mt">活动时间: {{ props.item.startTime }}</view>
-        </view>
-        <view class="flex justify-end base_p" style="width: 160rpx;">
-            <uni-tag
-                inverted
-                size="small"
-                :text="ACTIVITY_STATUS[props.item.status].text"
-                :type="ACTIVITY_STATUS[props.item.status].type"
-            ></uni-tag>
-            <uni-icons type="forward" size="20" color="#9CA3AF" style="margin-top: 5rpx;"></uni-icons>
-        </view>
+  <view class="base_item" @click="toActivityAttend">
+    <view class="base_p" style="width: 120rpx;">
+      <view class="base_icon">
+        <uni-icons type="flag-filled" size="30" class="m-auto"></uni-icons>
+      </view>
     </view>
+    <view class="flex flex-col base_p" style="width: 380rpx;">
+      <view class="base_head">[{{ props.item.type }}] {{ props.item.title }}</view>
+      <view class="text-gray-400 base_mt">活动时间: {{ props.item.startTime }}</view>
+    </view>
+    <view class="flex justify-end base_p" style="width: 160rpx;">
+      <uni-tag
+        inverted
+        size="small"
+        :text="ACTIVITY_STATUS[props.item.status].text"
+        :type="ACTIVITY_STATUS[props.item.status].type"
+      ></uni-tag>
+      <uni-icons type="forward" size="20" color="#9CA3AF" style="margin-top: 5rpx;"></uni-icons>
+    </view>
+  </view>
 </template>
 
 <script setup lang="ts">
@@ -28,6 +28,6 @@ import { ACTIVITY_STATUS } from '@/constant'
 const props = defineProps<{ item: activityItem }>()
 
 function toActivityAttend() {
-    uni.navigateTo({ url: '/pages/communist/activity/attend/index?id=' + props.item.id })
+  uni.navigateTo({ url: '/pages/communist/activity/attend/index?id=' + props.item.id })
 }
 </script>
--- src/components/share/contact-item.vue
+++ src/components/share/contact-item.vue
@@ -1,21 +1,21 @@
 <template>
-    <view class="flex base_p border_bottom" style="width: 710rpx;">
-        <uni-icons type="home-filled" size="30"></uni-icons>
-        <view class="flex flex-col base_ml">
-            <view style="font-size: 30rpx;">{{ props.item.name }}</view>
-            <view class="base_info">服务: {{ props.item.service }}</view>
-            <view class="flex base_info">
-                <view>电话:&nbsp;</view>
-                <view class="flex flex-col">
-                    <view v-for="tel in props.item.tels" :key="tel" class="flex">
-                        <view style="width: 480rpx;">{{ tel }}</view>
-                        <uni-icons type="phone-filled" size="30"></uni-icons>
-                    </view>
-                </view>
-            </view>
-            <view class="base_info">地址: {{ props.item.location }}</view>
+  <view class="flex base_p border_bottom" style="width: 710rpx;">
+    <uni-icons type="home-filled" size="30"></uni-icons>
+    <view class="flex flex-col base_ml">
+      <view style="font-size: 30rpx;">{{ props.item.name }}</view>
+      <view class="base_info">服务: {{ props.item.service }}</view>
+      <view class="flex base_info">
+        <view>电话:&nbsp;</view>
+        <view class="flex flex-col">
+          <view v-for="tel in props.item.tels" :key="tel" class="flex">
+            <view style="width: 480rpx;">{{ tel }}</view>
+            <uni-icons type="phone-filled" size="30"></uni-icons>
+          </view>
         </view>
+      </view>
+      <view class="base_info">地址: {{ props.item.location }}</view>
     </view>
+  </view>
 </template>
 
 <script setup lang="ts">
@@ -26,7 +26,7 @@ const props = defineProps<{ item: contactItem }>()
 
 <style scoped>
 .border_bottom {
-    @apply border-b border-solid border-gray-100;
-    @apply border-t-transparent border-l-transparent border-r-transparent;
+  @apply border-b border-solid border-gray-100;
+  @apply border-t-transparent border-l-transparent border-r-transparent;
 }
 </style>
